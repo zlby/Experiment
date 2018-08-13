d244a55e4f7b18d0fbaebb90cf092bdbb96e2a1e
Sammy Chou <zlby970129@gmail.com>
2018-08-13 20:19:30 +0100

    Update settings.py

--- Experiment/settings.py
+++ Experiment/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'experiment',
         'USER': 'root',
-        'PASSWORD': 'zlby8215468',
+        'PASSWORD': '*********',
         'HOST': '127.0.0.1',
     }
 }
